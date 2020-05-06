fb18da289d6e6b3d5026e32df26917ebbf6620f7
Punith <punith.jain@publicissapient.com>
2020-05-06 12:25:57 +0530

    first commit

---
+++ precache-manifest.a6f6cf736cc6d4ed595f84e9ae49cb2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d7ea076df3a4aeeda837ac59339cde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e8ea8180a2f01558503",
+    "url": "/static/css/main.c2a1bbba.chunk.css"
+  },
+  {
+    "revision": "91bc27fd50bf059a56bd",
+    "url": "/static/js/2.2e8e4155.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/static/js/2.2e8e4155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8ea8180a2f01558503",
+    "url": "/static/js/main.aa5a93e6.chunk.js"
+  },
+  {
+    "revision": "f8a70a92312e54852f13",
+    "url": "/static/js/runtime-main.eed1ad3b.js"
+  },
+  {
+    "revision": "f07a69d332342173bf21a9ea11a0a13e",
+    "url": "/static/media/landing-bg.f07a69d3.jpg"
+  },
+  {
+    "revision": "961e48ea6bd53487fc2fc7472ac92ac8",
+    "url": "/static/media/landing-bg2.961e48ea.jpg"
+  }
+]);
